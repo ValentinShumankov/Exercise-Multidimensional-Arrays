56e8bc8dad58c2774b302a5972a84eac2b358103
LEXXIM <71093361+LEXXIM@users.noreply.github.com>
2020-11-27 14:33:36 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string [ ] args)
+        {
+            int[] arr1 = new int[]{1,3};
+            int[] arr2 = new int[]{2};
+          double result =  FindMedianSortedArrays( arr2, arr2 );
+            Console.WriteLine(result);
+        }
+        public static double FindMedianSortedArrays(int [ ] nums1, int [ ] nums2)
+        {
+            for ( int i = 0; i < length; i++ )
+            {
+
+            }
+            return ( nums1.Skip(1).Sum() + nums2.SkipLast(1).Sum() / 2));
+        }
+    }
+}
